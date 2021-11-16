82efeaf0408913b5c3c883bb6cd66db46e17044e
sen95 <sen95@DESKTOP-GP05BES>
2021-11-16 16:55:03 +0900

    hello world

---
+++ src/hello2/Hello2Class.java
@@ -0,0 +1,20 @@
+package hello2;
+
+public class Hello2Class {
+	
+	public static void main(String[] args) {
+		// 안녕하세요 
+		// 나는 누구 입니다
+		// 잘 부탁드립니다
+		
+		String b = "이승현"; //문자열 변수 선언
+		int a = 27; // 정수 변수 선언
+		System.out.println("안녕하세요");
+		System.out.println("나는 " + b + " 입니다");
+		System.out.println("만나서 반갑습니다");
+		System.out.println("저는 " + a + "살 입니다");
+		System.out.println(b + " 인사 드립니다");
+		System.out.println("만나서 반갑습니다");
+	}
+
+}
