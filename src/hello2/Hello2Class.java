1ba23b5dbed5fe784795704572052e57a2b74ae5
oddity11 <sen9563@naver.com>
2021-11-17 14:19:02 +0900

    for

--- src/hello2/Hello2Class.java
+++ src/hello2/Hello2Class.java
@@ -1,29 +1,34 @@
 package hello2;
 
 public class Hello2Class {
+
+	    // 과제1
 	
+	    // 자기 소개
+
 	public static void main(String[] args) {
-		// 안녕하세요 
+		// 안녕하세요
 		// 나는 누구 입니다
 		// 잘 부탁드립니다
-		
-		String b = "이승현"; //문자열 변수 선언
-		int a = 27; 
-		
+
+		String b = "이승현"; // 문자열 변수 선언
+		int a = 27;
+
 		System.out.println("안녕하세요");
 		System.out.println("나는 " + b + " 입니다");
 		System.out.println("만나서 반갑습니다");
 		System.out.println("저는 " + a + "살 입니다");
 		System.out.println(b + " 인사 드립니다");
 		System.out.println("만나서 반갑습니다");
-		
+
+		// 과제2
+
 		// 제주도 3곳의 포토존과 여자친구의 사진 부탁
-		
-	    int place = 3;
-			
-		
+
+		int place = 3;
+
 		System.out.println("[제주도 여행 중 첫번째 포토존]");
-		
+
 		for (int i = 0; i < 5; i++) {
 			System.out.println("나는 열심히 " + (i + 1) + "번째 사진을 찍었다");
 			if (i == 4) {
@@ -35,10 +40,9 @@ public class Hello2Class {
 		System.out.print("남아있는 포토존은?");
 		place = place - 1;
 		System.out.println(place);
-		
-		
+
 		System.out.println("[제주도 여행 중 두번째 포토존]");
-		
+
 		for (int i = 0; i < 3; i++) {
 			System.out.println("나는 열심히 " + (i + 1) + "번째 사진을 찍었다");
 			if (i == 2) {
@@ -50,10 +54,9 @@ public class Hello2Class {
 		System.out.print("남아있는 포토존은?");
 		place = place - 1;
 		System.out.println(place);
-		
-		
+
 		System.out.println("[제주도 여행 중 세번째 포토존]");
-		
+
 		for (int i = 0; i < 8; i++) {
 			System.out.println("나는 열심히 " + (i + 1) + "번째 사진을 찍었다");
 			if (i == 7) {
@@ -62,9 +65,12 @@ public class Hello2Class {
 				System.out.println("사진이 여자친구 마음에 들지않는다");
 			}
 		}
+
+		//과제3
 		
+		// while 연습
+		
+	
 	}
 
-		
-	}
-
+}
